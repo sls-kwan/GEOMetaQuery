ac875fda1f92ea5ab98c794992279a7b6d4c7052
sls-kwan <shu.lun.shannon.kwan@gmail.com>
2015-10-09 16:32:46 +1100

    Adding new files

--- GEOgrab.R
+++ GEOgrab.R
@@ -8,7 +8,8 @@ library(gdata)
 
 if(!file.exists('GEOmetadb.sqlite')) getSQLiteFile()
 metadata <- dbConnect(SQLite(),'GEOmetadb.sqlite')
-
+dbListTables(metadata)
+dbListFields(metadata, 'gsm')
 GSESQLQuery <- c("select gse.gse", "gse.summary", "gse.title", "gse.overall_design",
                  "gse.pubmed_id","gse.title", "gse.supplementary_file", "gse.repeats", "gse.repeats_sample_list",
                  "gse.variable", "gse.variable_description")
@@ -23,11 +24,26 @@ grabgsm <- function(convgse){
   finalgsm <- as.vector(gsm)
   return(finalgsm$gsm)
 }
-
+grabgse <- function(convgsm){
+  gse<- dbGetQuery(metadata,paste("select gsm.series_id",
+                                   " from gsm",
+                                   " where gsm.gsm =\"", convgsm,"\"", "\n", sep=""))
+  finalgse <- as.vector(gse)
+  return(finalgse$series_id)
+}
 #Grabs Title and Charactersitics for GSM
 grabtitle <- function (gsmid){
-  titlenchar <- unlist(dbGetQuery(metadata,paste("select gsm.characteristics_ch1, gsm.title",
+  titlenchar <- unlist(dbGetQuery(metadata,paste("select gsm.characteristics_ch1, gsm.title, gsm.description, gsm.source_name_ch1",
                                                  " from gsm",
                                                  " where gsm.gsm =\"", gsmid,"\"", "\n", sep="")))
   return(titlenchar)
+}
+
+
+
+grabgpl <- function (gsmid){
+  gplid <- unlist(dbGetQuery(metadata,paste("select gsm.gpl",
+                                                 " from gsm",
+                                                 " where gsm.gsm =\"", gsmid,"\"", "\n", sep="")))
+  return(gplid)
 }
--- freetext_samples.R
+++ freetext_samples.R
@@ -7,68 +7,71 @@ library(gtools)
 library(textir)
 library(gdata)
 #####
-filenames = c("trainingoutp.csv",
-              "feats.txt")
+filenames = c("sample_types.csv",
+              "features_samples.txt")
 #####
 egfiles <- paste(getwd(), filenames[1], sep="/") ##Input has GSE
 featfile <- paste(getwd(), filenames[2], sep="/") ##Input has GSE
 if(!file.exists('GEOmetadb.sqlite')) getSQLiteFile()
-metadata <- dbConnect(SQLite(),'GEOmetadb.sqlite')
+  metadata <- dbConnect(SQLite(),'GEOmetadb.sqlite')
 
+GEOmetadata <- read.csv("~/COMP4930/GEOmetaScrape/GEOmetadata.txt", header=FALSE)
+GEOmetadata$V9 <- NULL
 trainingset <- read.csv(egfiles, header=FALSE)
-save(trainingset, file="~/COMP4930/Results/GSE/trainingset.RData")
+save(trainingset, file="Results/GSM/trainingset.RData")
 featset <- read.table(featfile, sep= ",", quote="\"")
 output <- list()
-gse <- as.vector.factor(trainingset$V1)
-gselist <- as.list(gse)
+gsm <- as.vector.factor(trainingset$V1)
+gsmlist <- as.list(gsm)
+rownames(GEOmetadata) <- gsm
 #gsmplatforms <- lapply(GSMList(gselist),function(x) {Meta(x)$platform})
 trainingset$row.names <- NULL
 #Function that grabs all corresponding GSE metadata
-graballinfo <- function(gseid) {
-  gsemeta <- unlist(dbGetQuery(metadata,paste(GSEQuery,
-                                              " from gse",
-                                              " where gse.gse =\"", gseid,"\"", "\n", sep="")))
-  gsetogsm<- lapply(gseid, grabgsm)
-  titlenchargsm <- unlist(lapply(as.list(unlist(gsetogsm)), grabtitle))
-  allinfo <- list(gsemeta,titlenchargsm)
-  return(allinfo)
+graballinfo <- function(gsmid) {
+  #gsemeta <- unlist(dbGetQuery(metadata,paste(GSEQuery,
+  #                                            " from gse",
+  #                                            " where gse.gse =\"", gseid,"\"", "\n", sep="")))
+  #gsetogsm<- lapply(gseid, grabgsm)
+  GSMrow <- which(rownames(GEOmetadata) == gsmid)
+  titlenchargsm <- GEOmetadata[GSMrow,]
+  titlenchargsm <- do.call(paste, titlenchargsm)
+  return(titlenchargsm)
 }
 
 
 
-metadata.all <- lapply(as.list(gse), graballinfo)
-save(metadata.all, file="~/COMP4930/Results/GSE/metadatafreetxt.RData")
+metadata.all <- lapply(as.list(gsm), graballinfo)
+save(metadata.all, file="Results/GSM/metadatafreetxt.RData")
 
 #Fowards each feature to be scored against GSE Metadata
 parsetoscore <- function (features, metatable){
-  metatable.score <- mapply(scoring, elementmeta = metatable, feature = features)
+  metatable.score <- mapply(scoring, elementmeta = metatable, feature = features, metaindex = seq_along(metatable))
   return(unlist(metatable.score))
 }
 
 #Scoring function for GSE Metadata
-scoring <- function(elementmeta, feature){
+scoring <- function(elementmeta, feature, metaindex){
   featCount <- sum(str_count(elementmeta, regex(feature, ignore_case = TRUE)))
   if(featCount> 0){
     freq <- featCount
-    return(as.numeric(freq))  ##Using Filename
+    return(as.numeric(freq))
   }
   
   return(as.numeric(0))
   
 }
 
-#Scoring function for GSE Metadata
-
 scoring.list <- lapply(as.list(as.character(featset$V1)), parsetoscore, metatable = metadata.all )
 scoring.matrix <- do.call(cbind, scoring.list)
 scoring.matrix <- tfidf(scoring.matrix)
 scoring.matrix[is.na(scoring.matrix)] <- 0
-rownames(scoring.matrix) <- gse
+rownames(scoring.matrix) <- gsm
 
 colnames(scoring.matrix) <- as.vector(featset$V1)
 
-save(scoring.matrix , file="~/COMP4930/Results/GSE/scoringmatrix.RData")
+save(scoring.matrix , file="Results/GSM/scoringmatrix.RData")
 
+#save occurence matrix
 dbDisconnect(metadata)
 
 ##TESTS
